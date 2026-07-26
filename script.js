4d08bdac64a1f36ffb1ad7b42ffb358315b61053
DhanGuruOfficial <dhanguruofficial0075@gmail.com>
2026-07-26 23:09:14 +0530

    Implement dynamic navigation style on scroll
    Add scroll event listener to change navigation style

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // Gurudev 007 Website
 console.log("Welcome to Gurudev 007 Official");
 console.log("Gurudev 007 Website Loaded");
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector("nav");
+
+if(window.scrollY>80){
+
+nav.style.background="rgba(0,0,0,.95)";
+nav.style.boxShadow="0 10px 30px rgba(0,0,0,.35)";
+
+}else{
+
+nav.style.background="rgba(0,0,0,.80)";
+nav.style.boxShadow="none";
+
+}
+
+});
